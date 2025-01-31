9fc173bc4eeca8f5219916a1f470f944872a5663
David Mazieres <dm@uun.org>
2025-01-30 19:37:48 -0800

    www deploy

---
+++ searchindex.en.js
@@ -0,0 +1,26 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Salty Plum",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "This is your new home page.",
+    "description": "This is your new home page.",
+    "tags": [],
+    "title": "Salty Plum",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "Salty Plum",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
